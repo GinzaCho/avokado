f8c7491079593c25ea038c1e6ab152d88298b80b
GinzaCho <55266901+GinzaCho@users.noreply.github.com>
2019-09-16 21:46:50 -0400

    Add files via upload

---
+++ avokado/avokado/DateCell.swift
@@ -0,0 +1,15 @@
+//
+//  DateCell.swift
+//  avokado
+//
+//  Created by Sangmin Cho on 9/13/19.
+//  Copyright © 2019 Sangmin Cho. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+
+class JTACDayCell : JTACDayCell {
+        @IBOutlet var dateLabel : UILabel!
+    }
---
+++ avokado/avokado/ViewController.swift
@@ -0,0 +1,34 @@
+import JTAppleCalendar
+import UIKit
+
+class DateCell: JTACDayCell {
+    @IBOutlet var dateLabel: UILabel!
+}
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension ViewController: JTACMonthViewDataSource {
+    func configureCalendar(_ calendar: JTACMonthView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        let startDate = formatter.date(from: "2018 01 01")!
+        let endDate = Date()
+        return ConfigurationParameters(startDate: startDate, endDate: endDate)
+    }
+}
+
+extension ViewController: JTACMonthViewDelegate {
+    func calendar(_ calendar: JTACMonthView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTACDayCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "dateCell", for: indexPath) as! DateCell
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
+    func calendar(_ calendar: JTACMonthView, willDisplay cell: JTACDayCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let cell = cell as! DateCell
+        cell.dateLabel.text = cellState.text
+    }
+}
